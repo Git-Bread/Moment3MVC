3235c3de1252b080bc6b593d43a09c772055205d
Max Gagner <m.gagner@hotmail.se>
2025-02-21 17:05:37 +0100

    cleaned up the code a bit, removed some redundacy and added possibility to add objects to the database

---
+++ Moment3MVC/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moment3MVC.Data;
+using Moment3MVC.Models;
+
+namespace Moment3MVC.Controllers
+{
+    public class BookController : Controller
+    {
+        //context "controller" (is that what you call it?) to interact with the database
+        private readonly BookDbContext _context;
+        public BookController(BookDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Index with books list
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Books.ToListAsync());
+        }
+
+        // GET: Books
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Title,Author,PublishedDate")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+    }
+}
--- Moment3MVC/Program.cs
+++ Moment3MVC/Program.cs
@@ -30,7 +30,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Main}/{action=Index}/{id?}");
+    pattern: "{controller=Book}/{action=Index}/{id?}");
 
 
 app.Run();
